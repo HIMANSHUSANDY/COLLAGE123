4f25d4b11f61c6fe851786036d70812e198b0d81
HIMANSHUSANDY <166990647+HIMANSHUSANDY@users.noreply.github.com>
2024-04-26 19:35:36 +0530

    Add files via upload
    create collage website using html css and some java script and also using php but some reason the email not get user

---
+++ formhandler.php
@@ -0,0 +1,20 @@
+<?php
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+
+$messege=$_POST['email'];
+$email_form='sandyhimanshu9@gmail.com';
+
+$email_subject='new form submission';
+
+$email_body="user name: $name.\n";
+
+$email_body="user name: $visitor_email.\n";
+$email_body="user name: $subject.\n";
+$email_body="user name: $message.\n";
+
+
+
+?>
+
